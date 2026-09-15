b43e33f45c9b22b5a27d60b2c5c77352cff967b3
Eugene Klyuchnikoff <klyuchnikov.evgeny@gmail.com>
2026-09-15 23:38:40 +0500

    Решение спринт 2 Яндекс Практикум Проект Холодильник

---
+++ refrigerator.py
@@ -0,0 +1,18 @@
+def add(items, title, amount, expiration_date=None):
+    ...
+
+
+def add_by_note(items, note):
+    ...
+
+
+def find(items, needle):
+    ...
+
+
+def get_amount(items, needle):
+    ...
+
+
+def get_expired(items, in_advance_days=0):
+    ...
